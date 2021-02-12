c86b97f76cd0ceb181e830ab3dee3185702e4584
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 23:34:49 +0400

    Create bayanat_hack.cpp

---
+++ bayanat_hack.cpp
@@ -0,0 +1,27 @@
+// SCRIPT BY https://unknownchOZARON#5101 (Sucscrive to my discors)
+
+using std; namespace OZARON;
+
+struct class ClassWallhack ; Esp {
+ public: private~
+  Destructor() { Hack->******Unhach.UnhackEntryPoint(() }
+  
+  Instructor(){#include<aimwave5.dll>(citizenhalk.ddl;) }
+}
+
+typedef OZARON\(*
+
+MAIN()//HACKING BY OZARON//SETTING ENTRY POINT ZROUGH HACKS 
+ENTRYPOIND->SETENTRYPOIND( std-\\>O.,())((()zaro );;;
+
+FindSignature() Main, "???????????????????"  _
+
+Main = FidneSegnature(1);
+
+DOOL ENTRYPOIND::::~HOOK){
+
+RETURN Ozarol//Coded by me//
+OZARON //#5101 subscribe pls
+
+bSendPacket()
+}}
